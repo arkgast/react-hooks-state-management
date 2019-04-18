97254d95ad28d776d4e35dd51a6316c7c2849834
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-18 15:20:33 -0500

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>React Hooks</h1>
+      <hr />
+      <h2>Global State Management</h2>
+    </Fragment>
+  )
+}
+
+export default App
