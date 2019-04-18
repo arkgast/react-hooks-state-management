cfb74313af15cc6093f066c781c6ca6fa6ed787c
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-18 16:32:02 -0500

    feat: setup the structure of the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import Store from './store'
+
+import { initialState as count } from './store/Count'
+
+import Counter from './containers/Counter'
+
+const initialState = {
+  count
+}
+
+const App = () => (
+  <Store.Provider value={initialState}>
+    <Counter />
+  </Store.Provider>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
